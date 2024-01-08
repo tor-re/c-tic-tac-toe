dbd5f477663cf9185b806a519732c16b163a40ff
tor-re <79610819+tor-re@users.noreply.github.com>
2024-01-08 09:41:19 -0700

    Update tictactoe.c
    refined code and made the system better

--- tictactoe.c
+++ tictactoe.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 void printboard(int a, char *arr[a]){
+    int count = 0;
     for(int i = 0; i < a; i++){
+        count++;
         printf("%s", arr[i]);
-        if(i==2 || i==5 || i==8){
-        printf("\n");
+        if(count>2){
+            printf("\n");
+            count = 0;
         }
+        
     }
 }
 int main()
 {
     int space = 0, gamerunning = 1;
     char *turn = "X";
-    char *board[9] = {"E","E","E","E","E","E","E","E","E"};
+    char *board[9] = {"□","□","□","□","□","□","□","□","□"};
     while(gamerunning){
         printboard(9, board);
         printf("player %s please enter a space [1 - 9]: ", turn);
         scanf("%d", &space);
-        if(board[space-1] == "E"){
+        if(board[space-1] == "□"){
             board[space - 1] = turn;
         }
         for(int i = 0; i < 5; i++){
@@ -24,65 +28,49 @@ int main()
         }
         if(board[0] == turn && board[1] == turn && board[2] == turn){
             gamerunning = 0;
-            board[0] = "-";
-            board[1] = "-";
-            board[2] = "-";
+            board[0] = board[1] = board[2] = "-";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[3] == turn && board[4] == turn && board[5] == turn){
             gamerunning = 0;
-            board[3] = "-";
-            board[4] = "-";
-            board[5] = "-";
+            board[3] = board[4] = board[5] = "-";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[6] == turn && board[7] == turn && board[8] == turn){
             gamerunning = 0;
-            board[6] = "-";
-            board[7] = "-";
-            board[8] = "-";
+            board[6] = board[7] = board[8] = "-";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[0] == turn && board[3] == turn && board[6] == turn){
             gamerunning = 0;
-            board[0] = "|";
-            board[3] = "|";
-            board[6] = "|";
+            board[0] = board[3] = board[6] = "|";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[1] == turn && board[4] == turn && board[7] == turn){
             gamerunning = 0;
-            board[1] = "|";
-            board[4] = "|";
-            board[7] = "|";
+            board[1] = board[4] = board[7] = "|";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[2] == turn && board[5] == turn && board[8] == turn){
             gamerunning = 0;
-            board[2] = "|";
-            board[5] = "|";
-            board[8] = "|";
+            board[2] = board[5] = board[8] = "|";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[0] == turn && board[4] == turn && board[8] == turn){
             gamerunning = 0;
-            board[0] = "\\";
-            board[4] = "\\";
-            board[8] = "\\";
+            board[0] = board[4] = board[8] = "\\";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
         } else if(board[6] == turn && board[4] == turn && board[2] == turn){
             gamerunning = 0;
-            board[2] = "/";
-            board[4] = "/";
-            board[6] = "/";
+            board[2] = board[4] = board[6] = "/";
             printf("\n");
             printboard(9, board);
             printf("win %s", turn);
